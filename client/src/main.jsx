c21547af9d39ca904f65e962815c6b08e212628b
jmo5896 <justin.m.moore5896@gmail.com>
2023-11-23 12:48:51 -0700

    adds registration/login forms

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import App from "./App.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  MDBContainer,
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+  MDBCheckbox,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 
-export default function Register() {
-  return <div>Register</div>;
+function Login() {
+  return (
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <h1>REGISTER</h1>
+
+      <MDBInput
+        autoComplete="true"
+        wrapperClass="mb-4"
+        label="Email"
+        id="email"
+        type="email"
+      />
+      <MDBInput
+        autoComplete="true"
+        wrapperClass="mb-4"
+        label="Password"
+        id="password"
+        type="password"
+      />
+
+      {/* <div className="d-flex justify-content-center mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              id="flexCheckDefault"
+              label="I have read and agree to the terms"
+            />
+          </div> */}
+
+      <MDBBtn className="mb-4 w-100">Sign up</MDBBtn>
+    </MDBContainer>
+  );
 }
+
+export default Login;
