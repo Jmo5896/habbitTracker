97a1ccaa01e338e649fa91cdd9c710762fcd9522
jmo5896 <justin.m.moore5896@gmail.com>
2024-01-17 14:48:49 -0700

    'prepull'

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <Row>
+        <Col>
+          <ul>
+            <li>add a habit</li>
+          </ul>
+        </Col>
+        <Col></Col>
+      </Row>
+    </>
+  );
 }
