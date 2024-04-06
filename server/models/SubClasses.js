0a64352c9e35493646388361ae651ebcb8933f90
jmo5896 <justin.m.moore5896@gmail.com>
2024-04-06 14:45:44 -0600

    adds more models and organization to the data models.  Putting a pin in figuring out how to track spells and spell slots

--- server/models/SubClasses.js
+++ server/models/SubClasses.js
@@ -5,6 +5,30 @@ const subClassSchema = new Schema({
     type: String,
     required: true,
   },
+  //   the index of feature levels will corelate with the features index
+  feautureLevels: [
+    {
+      type: Number,
+    },
+  ],
+  features: [
+    {
+      actionType: {
+        // action, bonus action, reaction, or static (permanent buff)
+        type: String,
+        required: true,
+      },
+      duration: {
+        // number of rounds feature lasts, 1 would be instant, 1 minute would be 10 rounds, -1 would be indefinite (permanent buffs)
+        type: Number,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
 const SubClass = model("SubClass", subClassSchema);
